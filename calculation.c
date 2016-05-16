52fac0a42d86fec80ffbeeb9380189cee90fbf3c
June Yang <yangjun4329@gmail.com>
2016-05-16 20:23:20 +0900

    My second commit!

---
+++ calculation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int x, y;
+	char e;
+
+	printf("두 수를 입력하시오:");
+	scanf("%d %d", &x, &y);
+
+	printf("연산자를 입력하시오:");
+	scanf("%c", &e);
+
+	if (c == '+')
+		printf("%d+%d=%d", a, b, a + b);
+}
